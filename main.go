ab0fdd59e3b01ce17482f984e9118299ca8d7e2c
Noah Kreiger <noahkreiger@gmail.com>
2022-05-19 00:08:28 -0500

    whoops lol

--- main.go
+++ main.go
@@ -343,7 +343,7 @@ func main() {
 	for i := 0; i < spec.NumberOfinvokers; i++ {
 		log.Println("Starting bootstrap", i+1)
 		go func(i int) {
-			cmd := exec.Command("sh", "-c", "/Users/noahkreiger/Documents/code-work/projects/badgercorp/infrastructure/aws-custom-runtime/_output/bootstrap")
+			cmd := exec.Command("sh", "-c", environment["LAMBDA_TASK_ROOT"]+"/bootstrap")
 			cmd.Env = append(os.Environ(), fmt.Sprintf("BOOTSTRAP_INDEX=%d", i))
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
